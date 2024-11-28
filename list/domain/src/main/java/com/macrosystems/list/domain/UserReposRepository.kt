e4e64e82da99d6b7b11305dcca889d6ae816a31d
Manuel Crovetto <87724520+ManuelCrovetto@users.noreply.github.com>
2024-11-28 22:35:38 +0100

    Renamed repository

--- list/domain/src/main/java/com/macrosystems/list/domain/UserReposRepository.kt
+++ list/domain/src/main/java/com/macrosystems/list/domain/UserReposRepository.kt
@@ -4,7 +4,7 @@ import com.macrosystems.core.domain.RepoData
 import com.macrosystems.core.domain.Result
 import com.macrosystems.core.domain.util.DataError
 
-interface UserGithubRepository {
+interface UserReposRepository {
 
     suspend fun fetchRepositories(user: String): Result<List<RepoData>, DataError.Network>
 
